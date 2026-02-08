b1b4cc830de5a6ba0e8205e8516f247c404165c0
Harsh <harshkparikh1813@gmail.com>
2026-02-08 16:06:05 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot-with-llms-langchain-pinecone-flask-aws",
+    version="0.1.0",
+    author="Harsh Parikh",
+    author_email="harshkparikh1813@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
