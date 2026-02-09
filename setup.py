2f3b324256ed52265622f5d4feb44e592baa77ac
Harsh <harshkparikh1813@gmail.com>
2026-02-08 21:18:36 -0400

    DOC update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medical-chatbot-with-llms-langchain-pinecone-flask-aws",
+    name="medical-chatbot",
     version="0.1.0",
     author="Harsh Parikh",
     author_email="harshkparikh1813@gmail.com",
